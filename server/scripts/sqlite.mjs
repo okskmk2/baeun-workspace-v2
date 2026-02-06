fc024335a63f2621d6a723d18d0e42d4f3a2bd67
Eunsung Lee <okskmk2@bdinc.co.kr>
2026-02-06 16:19:48 +0900

    1

--- server/scripts/sqlite.mjs
+++
@@ -1,5 +0,0 @@
-import sql from "better-sqlite3";
-
-const db = sql("baeun-workspace.sqlite");
-const row = db.prepare("SELECT * FROM users WHERE id = ?").get(userId);
-console.log(row.firstName, row.lastName, row.email);
